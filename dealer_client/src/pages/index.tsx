a64a89711dceb526438bb977094d7ddabea4625f
jtamedrano <jtamedrano@gmail.com>
2021-11-15 21:21:09 -0800

    corrected overlay issue in public layout

--- dealer_client/src/pages/index.tsx
+++ dealer_client/src/pages/index.tsx
@@ -2,9 +2,28 @@ import React, { FC } from 'react'
 
 import { Public } from '../layout'
 
+const HeroOverlay = () => {
+  return (
+    <>
+      <div aria-hidden="true" className="absolute inset-0 overflow-hidden">
+        <img
+          // src="https://images.unsplash.com/photo-1606423258821-621996281c0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          src="https://images.unsplash.com/photo-1634981297356-f9b4daa98a92?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+          alt="Care Dealership with people shopping in the corner"
+          className="w-full h-full object-center object-cover"
+        />
+      </div>
+      <div
+        aria-hidden="true"
+        className="absolute inset-0 bg-gray-900 opacity-50"
+      />
+    </>
+  )
+}
+
 const index: FC = () => {
   return (
-    <Public>
+    <Public heroOverlay={<HeroOverlay />}>
       <div className="relative max-w-3xl mx-auto py-32 px-6 flex flex-col items-center text-center sm:py-64 lg:px-0">
         <h1 className="text-4xl font-extrabold tracking-tight text-white lg:text-6xl">
           Automodiv
