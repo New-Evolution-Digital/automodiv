701771f9fba0058aebd4e11d9925e659732f1ff4
PLisak777 <phil.lisak@gmail.com>
2021-12-01 21:28:55 -0700

    Initial shopper setup cont.

---
+++ shopper/components/cardContainer.js
@@ -0,0 +1,14 @@
+import Card from '../components/card';
+
+const CardContainer = () => {
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+export default CardContainer;
